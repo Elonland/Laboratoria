534adb246b3e1b989ab21679e85c844fe397be81
Elon_Land <33283599+Elonland@users.noreply.github.com>
2022-04-06 12:46:30 +0200

    Delete Lista 2 directory

--- Lista 2/MyException.cpp
+++
@@ -1,8 +0,0 @@
-/*#include "MyException.hpp"
-#include <iostream>
-
-MyException::MyException(const std::string& msg) noexcept(true)
-: std::runtime_error(msg.c_str())
-{
-	std::cout << msg.c_str() << std::endl;
-}*/
